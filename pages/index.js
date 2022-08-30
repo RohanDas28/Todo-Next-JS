ecc0eadc714fdd22d64c2313c75b66984c3f7b93
Rohan Das <rohandasbirbhum@gmail.com>
2022-08-31 01:40:23 +0530

    Version 1

--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,27 @@ import Navbar from "../components/Navbar";
 export default function Home() {
   const current = new Date();
 
-  let date = `${current.getHours()}:${current.getMinutes()}:${current.getSeconds()}`
+  let options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  // var date  = current.toLocaleDateString("en-US", options)
+  let date = current.toLocaleDateString("hi-IN", options);
 
+  // let date = `${current.getDay()}/${current.getMonth()}/${getYear()} ${current.getHours()}:${current.getMinutes()}`
   const [todoItem, setTodoItem] = useState({});
   const [items, setItems] = useState([]);
 
   const handleChange = (e) => {
-    setTodoItem({ ...todoItem, [e.target.name]: e.target.value })
-    console.log(todoItem)
-  }
+    setTodoItem({ ...todoItem, [e.target.name]: e.target.value });
+    console.log(todoItem);
+  };
 
   const handleAdd = () => {
     if (todoItem.text && todoItem.desc) {
-      console.log('first')
+      console.log("first");
       // if the input is not empty
       let data = [
         {
@@ -29,10 +37,10 @@ export default function Home() {
           done: false,
         },
         ...items,
-      ]
+      ];
       setItems(data); // add the input to the array
 
-      localStorage.setItem('todo', JSON.stringify(data))
+      localStorage.setItem("todo", JSON.stringify(data));
       setTodoItem({}); // reset the input
     }
   };
@@ -48,61 +56,61 @@ export default function Home() {
       return item;
     });
 
-    localStorage.setItem('todo', JSON.stringify(_items))
+    localStorage.setItem("todo", JSON.stringify(_items)); // update the local storage
     setItems(_items);
-
   };
 
   const handleDelete = (id) => {
+    const data = JSON.parse(JSON.stringify(items));
 
-    const data = JSON.parse(JSON.stringify(items))
-
-    const newData = []
+    const newData = [];
 
     data.map((item) => {
       if (item.id != id) {
-        newData.push(item)
+        newData.push(item);
       }
-      
     });
 
-    localStorage.setItem('todo', JSON.stringify(newData))
+    localStorage.setItem("todo", JSON.stringify(newData));
     setItems(newData);
-  }
+  };
 
   useEffect(() => {
-    let data = localStorage.getItem('todo')
+    let data = localStorage.getItem("todo");
     if (data) {
-      setItems(JSON.parse(data))
+      setItems(JSON.parse(data));
     }
-  }, [])
-
+  }, []);
 
   return (
     <>
-      <Head />
       <div className="min-vh-100">
-
         <Navbar />
         <div className="d-flex justify-content-center p-4">
           <h1>ToDo App</h1>
         </div>
         <div className="d-flex justify-content-center p-2">
           <input
+            placeholder="Enter your task"
             type="text"
             name="text"
-            value={todoItem.text || ''}
+            value={todoItem.text || ""}
             onChange={handleChange}
             className="p-2 m-2"
           />
           <input
+            placeholder="Enter your description"
             type="text"
             name="desc"
-            value={todoItem.desc || ''}
+            value={todoItem.desc || ""}
             onChange={handleChange}
             className="p-2 m-2"
           />
-          <button type="button" onClick={handleAdd} className="p-2 m-2">
+          <button
+            className=" p-2 m-2 btn btn-primary"
+            type="button"
+            onClick={handleAdd}
+          >
             Add
           </button>
         </div>
@@ -113,43 +121,63 @@ export default function Home() {
               .map(({ id, text, done, date, desc }) => (
                 <li
                   key={id}
-                  style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    marginTop: "10px",
+                  }}
                 >
-                  <div style={{ marginRight: '40px' }} onClick={() => handleToggle(id)}>
+                  <div
+                    style={{ marginRight: "40px" }}
+                    onClick={() => handleToggle(id)}
+                  >
                     <div className="">
-
                       <b>{text}</b> {date}
                     </div>
-                    <div className="">
-                      {desc}
-
-                    </div>
-
+                    <div className="">{desc}</div>
                   </div>
-                  <button onClick={() => { handleDelete(id) }}>Delete</button>
+                  <button
+                    className=" btn btn-sm btn-danger"
+                    onClick={() => {
+                      handleDelete(id);
+                    }}
+                  >
+                    Delete
+                  </button>
                 </li>
               ))}
           </ul>
-          <hr style={{ width: '35%' }} />
+          <hr style={{ width: "35%" }} />
           <ul>
             {items
               .filter(({ done }) => done)
               .map(({ id, text, done, date, desc }) => (
                 <li
                   key={id}
-                  style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    marginTop: "10px",
+                  }}
                 >
-                  <div style={{ marginRight: '40px' }} onClick={() => handleToggle(id)} className={`${done ? "text-decoration-line-through" : " "}`}>
+                  <div
+                    style={{ marginRight: "40px" }}
+                    onClick={() => handleToggle(id)}
+                    className={`${done ? "text-decoration-line-through" : " "}`}
+                  >
                     <div className="">
                       <b>{text}</b> {date}
                     </div>
-                    <div className="">
-                      {desc}
-
-                    </div>
-
+                    <div className="">{desc}</div>
                   </div>
-                  <button onClick={() => { handleDelete(id) }}>Delete</button>
+                  <button
+                    className="btn btn-sm btn-danger"
+                    onClick={() => {
+                      handleDelete(id);
+                    }}
+                  >
+                    Delete
+                  </button>
                 </li>
               ))}
           </ul>
